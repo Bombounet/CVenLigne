735f6c1fedbab272f7770ecf31c37940a3e5ff5a
masse aurelien <masseaurelien@MacBook-Air-de-masse.local>
2019-09-16 14:51:19 +0200

    creation table et premier user

--- src/main/java/fr/cv/User.kt
+++ src/main/java/fr/cv/User.kt
@@ -1,11 +1,38 @@
 package fr.cv
 
+import javax.persistence.*
+
 /**
  *
  */
-data class User(var firstName: String?, var lastName: String?, var birthMonth: Int?, var birthDay: Int?,
-                var birthYear: Int?, var email: String?, var adresse: Adresse?,var phoneNumber: Int?,var experiences: List<Experience>?,
-                var formations: List<Formation>?, var projets: List<Projet>?, var competences: List<String>?) {
+@Entity
+
+data class User(
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Id var id: Long?,
+        var firstName: String?,
+        var lastName: String?,
+        var birthMonth: Int?,
+        var birthDay: Int?,
+        var birthYear: Int?,
+        var email: String?,
+
+        @Embedded
+        var adresse: Adresse?,
+
+        var phoneNumber: Int?,
+
+        @OneToMany
+        var experiences: List<Experience>?,
+
+        @OneToMany
+        var formations: List<Formation>?,
+
+        @OneToMany
+        var projets: List<Projet>?,
+
+        @ManyToMany
+        var competences: List<String>?) {
 
 
 }
