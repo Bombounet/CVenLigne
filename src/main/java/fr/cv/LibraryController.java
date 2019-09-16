75f053cc3d527992632203b84c4ca158b80f2b14
Romain Galmes <romain.galmes@epfedu.fr>
2019-09-16 18:41:51 +0200

    debut edit

--- src/main/java/fr/cv/LibraryController.java
+++ src/main/java/fr/cv/LibraryController.java
@@ -16,52 +16,104 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 public class LibraryController {
 
-  private final UserDAO userDAO;
+    private final UserDAO userDAO;
 
-  public LibraryController(UserDAO userDAO) {
-    this.userDAO = userDAO;
-  }
+    public LibraryController(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
 
-  @GetMapping
-  public String homePage(Model m) {
-    m.addAttribute("user", userDAO.getUser());
-    return "index";
-  }
+    @GetMapping
+    public String homePage(Model m) {
+        m.addAttribute("user", userDAO.getUser());
+        return "see_index";
+    }
 
-  @GetMapping("/contact")
-  public String seeContact(Model m) {return "contact"; }
 
-  @PostMapping("/contact")
-  public RedirectView closeContact() {return new RedirectView("/"); }
 
-  @GetMapping("/formations")
-  public String seeFormations() {return "formations";}
+    @GetMapping("/edit")
+    public String addUserPage(Model m) {
+        m.addAttribute("user", userDAO.getUser());
+        return "edit_index";
+    }
 
-  @PostMapping("/formations")
-  public RedirectView quitFormations() {return new RedirectView("/");}
+    @PostMapping("/edit")
+    public RedirectView createNewUser(@ModelAttribute User user, RedirectAttributes attrs) {
+        userDAO.changeInfoIndex(user);
+        return new RedirectView("/");
+    }
 
-  @GetMapping("/experiences")
-  public String seeExperiences() {return "experiences";}
 
-  @PostMapping("/experiences")
-  public RedirectView quitExperiences() {return new RedirectView("/");}
 
-  @GetMapping("/competences")
-  public String seeCompetences() {return "competences";}
 
-  @PostMapping("/competences")
-  public RedirectView quitCompetences() {return new RedirectView("/");}
+/*
+    @GetMapping("/edit")
+    public String homePageEdit(Model m) {
+        m.addAttribute("user", userDAO.getUser());
+      System.out.println("laaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        return "edit_index";
+    }
 
-  @GetMapping("/projets")
-  public String seeProjets() {return "projets";}
 
-  @PostMapping("/projets")
-  public RedirectView quitProjet() {return new RedirectView("/");}
-
-  @GetMapping("/edit")
-  public String seeEdit() {return "edit_index";}
 
   @PostMapping("/edit")
-  public RedirectView quitEdit() {return new RedirectView("/");}
+  public RedirectView homePageEditSaveInfo(@ModelAttribute User user, RedirectAttributes attrs) {
+      attrs.addFlashAttribute("message", "Utilisateur ajouté avec succès");
+        System.out.println("liiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+    userDAO.changeInfo(user);
+    return new RedirectView("/");
+  }*/
+
+
+
+
+    @GetMapping("/contact")
+    public String seeContact(Model m) {
+        return "contact";
+    }
+
+    @PostMapping("/contact")
+    public RedirectView closeContact() {
+        return new RedirectView("/");
+    }
+
+    @GetMapping("/formations")
+    public String seeFormations() {
+        return "formations";
+    }
+
+    @PostMapping("/formations")
+    public RedirectView quitFormations() {
+        return new RedirectView("/");
+    }
+
+    @GetMapping("/experiences")
+    public String seeExperiences() {
+        return "experiences";
+    }
+
+    @PostMapping("/experiences")
+    public RedirectView quitExperiences() {
+        return new RedirectView("/");
+    }
+
+    @GetMapping("/competences")
+    public String seeCompetences() {
+        return "competences";
+    }
+
+    @PostMapping("/competences")
+    public RedirectView quitCompetences() {
+        return new RedirectView("/");
+    }
+
+    @GetMapping("/projets")
+    public String seeProjets() {
+        return "projets";
+    }
+
+    @PostMapping("/projets")
+    public RedirectView quitProjet() {
+        return new RedirectView("/");
+    }
 
 }
--- src/main/java/fr/cv/UserDAO.java
+++ src/main/java/fr/cv/UserDAO.java
@@ -3,6 +3,7 @@ package fr.cv;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
+import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,7 +13,8 @@ import java.util.List;
  */
 @Repository
 public class UserDAO {
-  private List<User> users = new ArrayList<>();
+
+  private static List<User> users = new ArrayList<>();
 
   public void add(User u) {
     users.add(u);
@@ -33,6 +35,8 @@ public class UserDAO {
     List<String> competences = new ArrayList<>();
     experiences.add(new Experience("Space engineer","2015-2020","NASA","i went to mars (2 times)"));
     formations.add(new Formation("ENS Cachan", "2014", "Cachan","major student"));
+    formations.add(new Formation("EPF", "2015", "Sceaux","it"));
+    formations.add(new Formation("Harvard", "2020", "US","prout"));
     projets.add(new Projet("Jeu en Java","2017","Montpellier EPF","we code a modified version of binding of Isaac named Binding of the PEUF, we code more than 3 days in a row"));
     competences.add("Anglais F1");
     competences.add("Espagnol F1");
@@ -45,4 +49,10 @@ public class UserDAO {
             new Adresse("21 rue de la porte du passée","viennes",34250,"FRANCE") ,610798624 ,
              experiences,  formations,projets,competences ));
   }
+
+  public void changeInfoIndex(User newUser) {
+    users.get(0).setFirstName(newUser.getFirstName());
+    users.get(0).setLastName(newUser.getLastName());
+  }
+
 }
