f1349e24a16158fb4ca16ec9a1bc239b812678d3
Romain Galmes <romain.galmes@epfedu.fr>
2019-09-17 10:58:02 +0200

    v0.3

--- src/main/java/fr/cv/UserDAO.java
+++ src/main/java/fr/cv/UserDAO.java
@@ -33,6 +33,8 @@ public class UserDAO {
     List<String> competences = new ArrayList<>();
     experiences.add(new Experience("Space engineer","2015-2020","NASA","i went to mars (2 times)"));
     formations.add(new Formation("ENS Cachan", "2014", "Cachan","major student"));
+    formations.add(new Formation("ENS Cachan", "2014", "Cachan","major student"));
+    formations.add(new Formation("EPF", "2015", "Sceaux","it"));
     projets.add(new Projet("Jeu en Java","2017","Montpellier EPF","we code a modified version of binding of Isaac named Binding of the PEUF, we code more than 3 days in a row"));
     competences.add("Anglais F1");
     competences.add("Espagnol F1");
@@ -41,8 +43,40 @@ public class UserDAO {
     competences.add("Java");
     competences.add("C");
     competences.add("IA");
-    users.add(new User(1,"Loïc", "Ortola", 8, 10,1997, "toto@hotmail.com" ,
+    long id = 1;
+    users.add(new User(id,"Loïc", "Ortola", 8, 10,1997, "toto@hotmail.com" ,
             new Adresse("21 rue de la porte du passée","viennes",34250,"FRANCE") ,610798624 ,
              experiences,  formations,projets,competences ));
   }
+
+  public void changeInfoIndex(User newUser) {
+    users.get(0).setFirstName(newUser.getFirstName());
+    users.get(0).setLastName(newUser.getLastName());
+  }
+
+  public void changeInfoContact(User newUser) {
+    users.get(0).setFirstName(newUser.getFirstName());
+    users.get(0).setLastName(newUser.getLastName());
+  }
+
+  public void changeInfoFormations(User newUser) {
+    users.get(0).setFirstName(newUser.getFirstName());
+    users.get(0).setLastName(newUser.getLastName());
+  }
+
+  public void changeInfoExperiences(User newUser) {
+    users.get(0).setFirstName(newUser.getFirstName());
+    users.get(0).setLastName(newUser.getLastName());
+  }
+
+  public void changeInfoCompetences(User newUser) {
+    users.get(0).setFirstName(newUser.getFirstName());
+    users.get(0).setLastName(newUser.getLastName());
+  }
+
+  public void changeInfoProjets(User newUser) {
+    users.get(0).setFirstName(newUser.getFirstName());
+    users.get(0).setLastName(newUser.getLastName());
+  }
+
 }
