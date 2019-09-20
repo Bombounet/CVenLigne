732d39d446d4b9e521e43e54f03ea3d0331237a8
MarionDelort <marion.delort@epfedu.fr>
2019-09-20 13:55:13 +0200

    pour aurelien le païen

--- src/main/java/fr/cv/Experience.kt
+++ src/main/java/fr/cv/Experience.kt
@@ -1,9 +1,10 @@
-package fr.cv
+package experiencesfr.cv
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import fr.cv.User
 import javax.persistence.*
 
-@Entity(name ="experiences")
+@Entity(name ="")
 data class Experience(
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Id var id: Long?,
@@ -11,11 +12,8 @@ data class Experience(
         var year: String?,
         var place: String?,
         var description: String?,
-
         @ManyToOne
-        var user: User?
-){
-
+        var user: User?){
         constructor() : this(null,null,null,null,null, null)
 
 }
